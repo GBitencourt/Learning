b16336fca17868b512a2da123adfd058610e5c43
GBitencourt <hodzim@gmail.com>
2015-06-18 19:10:03 -0300

    Adicionei o retangulo com sucesso

--- OPrimeiroDeMuitos/mainwindow.cpp
+++ OPrimeiroDeMuitos/mainwindow.cpp
@@ -1,5 +1,10 @@
 #include <QDebug>
 #include "mainwindow.h"
+#include <QGraphicsScene>
+
 mainwindow::mainwindow() {
+    QGraphicsScene *minhacena = new QGraphicsScene();
+    setScene(minhacena);
+    minhacena->addRect(20,50,20,50);
     qDebug()<< "Teste 2";
 }
