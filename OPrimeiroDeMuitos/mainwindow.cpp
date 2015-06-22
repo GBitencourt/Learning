7699dd3b983773d5c8c23dd5d6edadee098a96bd
GBitencourt <hodzim@gmail.com>
2015-06-22 16:23:04 -0300

    adicionei o addtext que nao funciona

--- OPrimeiroDeMuitos/mainwindow.cpp
+++ OPrimeiroDeMuitos/mainwindow.cpp
@@ -6,5 +6,6 @@ mainwindow::mainwindow() {
     QGraphicsScene *minhacena = new QGraphicsScene();
     setScene(minhacena);
     minhacena->addRect(20,50,20,50);
+    minhacena->addText("Teste de texto na caixa yay")
     qDebug()<< "Teste 2";
 }
