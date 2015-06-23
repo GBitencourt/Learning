31bc000eb48bd2cd7a2f58ccc08253432640956b
GBitencourt <hodzim@gmail.com>
2015-06-23 14:23:47 -0300

    Adicionado o for e funcionando

--- OPrimeiroDeMuitos/mainwindow.cpp
+++ OPrimeiroDeMuitos/mainwindow.cpp
@@ -1,3 +1,5 @@
+//Aula de c++ episódio atual: https://www.youtube.com/watch?v=t9iyPMyMjrg&list=PLesCEcYj003QTw6OhCOFb1Fdl8Uiqyrqo&index=16
+
 #include <QDebug>
 #include "mainwindow.h"
 #include <QGraphicsScene>
@@ -5,7 +7,12 @@
 mainwindow::mainwindow() {
     QGraphicsScene *minhacena = new QGraphicsScene();
     setScene(minhacena);
-    minhacena->addRect(20,50,20,50);
-    minhacena->addText("Teste de texto na caixa yay")
-    qDebug()<< "Teste 2";
+    //minhacena->addRect(20,50,20,50);
+    for( int i = 0; i < 8; i = i + 1 ) {
+            for ( int x = 0; x < 8; x = x + 1 ) {
+            minhacena->addRect(i*50,x*50,50,50);
+            qDebug() << "Teste de For:" << i << "e" << x ;
+            }
+    }
+    //qDebug()<< "Teste 2";
 }
