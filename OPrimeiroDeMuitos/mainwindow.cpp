fd30aad7e7604ff0e28ff863d406db89aa4c54e7
GBitencourt <hodzim@gmail.com>
2015-06-23 16:19:35 -0300

    Adicionei if e else com as cores para pintar os quadrados

--- OPrimeiroDeMuitos/mainwindow.cpp
+++ OPrimeiroDeMuitos/mainwindow.cpp
@@ -3,6 +3,9 @@
 #include <QDebug>
 #include "mainwindow.h"
 #include <QGraphicsScene>
+#include "QColor"
+#include "QBrush"
+#include "QGraphicsRectItem"
 
 mainwindow::mainwindow() {
     QGraphicsScene *minhacena = new QGraphicsScene();
@@ -10,7 +13,21 @@ mainwindow::mainwindow() {
     //minhacena->addRect(20,50,20,50);
     for( int i = 0; i < 8; i = i + 1 ) {
             for ( int x = 0; x < 8; x = x + 1 ) {
-            minhacena->addRect(i*50,x*50,50,50);
+            QGraphicsRectItem *meuRect=minhacena->addRect(i*50,x*50,50,50);
+            QColor preto(0,0,0);
+            QColor branco(255,255,255);
+            QBrush meuBrush;
+            int y = ( x + i ) ;
+            if ( y % 2 == 0) {
+                meuBrush.setColor(preto);
+                meuRect->setBrush(meuBrush);
+                qDebug() << "+Pretim" ;
+            }
+            else {
+                meuBrush.setColor(branco);
+                meuRect->setBrush(meuBrush);
+                qDebug() << "-Braquim";
+            }
             qDebug() << "Teste de For:" << i << "e" << x ;
             }
     }
